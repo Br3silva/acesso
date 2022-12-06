6959610cbac6bd6d00ec308905105b149dd21290
br3silva <119340372+Br3silva@users.noreply.github.com>
2022-12-06 16:44:00 -0300

    conexao locahost

---
+++ config.php
@@ -0,0 +1,21 @@
+
+
+<?php    
+$dbhost='localhost';
+$dbUsername='root';
+$dbPassword='';
+$dbName='bd_relatorio';
+
+$conexao =new mysqli ($dbhost,$dbUsername,$dbPassword,$dbName);
+
+if ($conexao -> connect_errno ){
+
+    echo"erro";
+}
+else{
+
+    echo "conexao bem sucedida";
+}
+
+
+?>
