b4ba11743ce06b832e2f878e9d0824f6228a0218
br3silva <119340372+Br3silva@users.noreply.github.com>
2022-12-06 17:12:54 -0300

    COMUNICAÇAO COM BD

---
+++ formulario.php
@@ -0,0 +1,53 @@
+
+<?php
+if(isset($_POST['submit']))
+{
+
+    print_r($_POST['usuario']);
+    print_r($_POST['senha']);
+   
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
+<div>
+    <div class="container_login">
+        <div class="areaLogin">
+            <h2>Acesso</h2>
+        <form action="formulario.php" name="form" method="POST">
+        <label for="usuario">usuario</label>
+        <input type="text" name="usuario" type="button" value="">
+        <label for="senha">senha</label>
+        <input type="text" name="senha" type="button" value=""><br>
+        <button type="submit"  >Enviar</button>
+    </form>
+    </div>
+    </div>
+
+
+
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+    <script src="./index.js" ></script>
+</body>
+</html>
