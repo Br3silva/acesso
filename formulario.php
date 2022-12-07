d0b67504679a709db3de06fdadf8a5eacc894e0e
br3silva <119340372+Br3silva@users.noreply.github.com>
2022-12-07 10:33:33 -0300

    instalado extensao php

--- formulario.php
+++ formulario.php
@@ -1,17 +1,3 @@
-
-<?php
-if(isset($_POST['submit']))
-{
-
-    $email = $_GET['usuario'];
-    $senha = $_POST['senha'];
-
-    print_r('Email: ' . $email);
-    print_r('<br>');
-    print_r('Senha: ' . $senha);
-}
-
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,16 +8,27 @@ if(isset($_POST['submit']))
     <link rel="stylesheet" href="./style.css">
 </head>
 <body>
+<?php
+if(isset($_POST['submit']))
+{
+
+    $email = $_POST['usuario'];
+    $senha = $_POST['senha'];
+
+    print_r($email);
+    print_r( $senha );
+}
+?>
     <div>
         <div class="container_login">
             <div class="areaLogin">
             <h2>Acesso</h2>
-        <form action="formulario.php" name="form" methosd="GET">
+        <form action="formulario.php" name="form" methossd="post">
         <label for="usuario">usuario</label>
         <input type="text" name="usuario" type="button" value="">
         <label for="senha">senha</label>
         <input type="text" name="senha" type="button" value=""><br>
-        <button type="submit"  >Enviar</button>
+        <button type="submit">Enviar</button>
             </form>
         </div>
     </div>
